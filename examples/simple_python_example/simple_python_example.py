513d0c3dd014256eaba99d890528284cc6725b44
Nir Geller <gnir.work@gmail.com>
2024-07-12 23:04:50 +0300

    Updated example code

--- examples/simple_python_example/simple_python_example.py
+++ examples/simple_python_example/simple_python_example.py
@@ -8,7 +8,7 @@ logger = logging.getLogger("simple_python_example")
 
 
 async def main() -> None:
-    async def sleep(coroutine_id: int) -> None:
+    async def non_blocking_sleep(coroutine_id: int) -> None:
         logger.info(f"[id: {coroutine_id}] Before non-blocking sleep")
         await asyncio.sleep(1)
         logger.info(f"[id: {coroutine_id}] After non-blocking sleep")
@@ -19,23 +19,25 @@ async def main() -> None:
         logger.info(f"[id: {coroutine_id}] After blocking sleep")
 
     await asyncio.gather(
-        sleep(coroutine_id=0),
+        non_blocking_sleep(coroutine_id=0),
         blocking_sleep(coroutine_id=1),
-        sleep(coroutine_id=2),
-        sleep(coroutine_id=3),
+        non_blocking_sleep(coroutine_id=2),
+        non_blocking_sleep(coroutine_id=3),
         blocking_sleep(coroutine_id=4),
-        sleep(coroutine_id=5),
+        non_blocking_sleep(coroutine_id=5),
     )
 
 
 def monitor(ioloop_monitor_state: IoLoopMonitorState) -> None:
     if ioloop_monitor_state.callback_wall_time > 0.1:
         logger.warning(
-            f"Blocking operation detected, executing took: {ioloop_monitor_state.callback_wall_time}"
+            f"Blocking operation detected, executing of {ioloop_monitor_state.callback_pretty_name} "
+            f"took: {ioloop_monitor_state.callback_wall_time}"
         )
     if ioloop_monitor_state.loop_lag > 0.1:
         logger.warning(
-            f"A task was executed after a significant delay: {ioloop_monitor_state.loop_lag}"
+            f"Task {ioloop_monitor_state.callback_pretty_name} was executed after "
+            f"a significant delay: {ioloop_monitor_state.loop_lag}"
         )
 
 
